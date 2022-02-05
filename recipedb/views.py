6ec912a04472043ad7c8a0b7580f42eb802974f2
Christian Theune <ct@flyingcircus.io>
2022-02-05 07:26:41 +0100

    black

--- recipedb/views.py
+++ recipedb/views.py
@@ -3,44 +3,41 @@ from django.views import generic
 import datetime
 
 DAY_MAPPING = {
-    0: 'Montag',
-    1: 'Dienstag',
-    2: 'Mittwoch',
-    3: 'Donnerstag',
-    4: 'Freitag',
-    5: 'Samstag',
-    6: 'Sonntag'}
+    0: "Montag",
+    1: "Dienstag",
+    2: "Mittwoch",
+    3: "Donnerstag",
+    4: "Freitag",
+    5: "Samstag",
+    6: "Sonntag",
+}
 
 EPOCH = datetime.date(2018, 1, 4)  # First week 1
 
 
 class IndexView(generic.TemplateView):
-    """The "infinite menu".
+    """The "infinite menu"."""
 
-    """
-
-    template_name = 'recipes/index.html'
+    template_name = "recipes/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        recipes = list(Recipe.objects.order_by('id').all())
+        recipes = list(Recipe.objects.order_by("id").all())
 
         weeks_since_epoch = (datetime.date.today() - EPOCH).days / 7
         current_week = int(int(weeks_since_epoch) % (len(recipes) / 7)) + 1
-        context['current_week'] = current_week
-        context['weeks'] = []
+        context["current_week"] = current_week
+        context["weeks"] = []
         week = None
         day = 0
         while recipes:
             if not week:
-                week = {'weeknumber': len(context['weeks']) + 1, 'days': []}
-            week['is_current'] = week['weeknumber'] == current_week
-            week['days'].append({
-                'day': DAY_MAPPING[day],
-                'recipe': recipes.pop(0)})
+                week = {"weeknumber": len(context["weeks"]) + 1, "days": []}
+            week["is_current"] = week["weeknumber"] == current_week
+            week["days"].append({"day": DAY_MAPPING[day], "recipe": recipes.pop(0)})
             day += 1
             if day == 7:
-                context['weeks'].append(week)
+                context["weeks"].append(week)
                 week = None
                 day = 0
         return context
